e28165ffa27f1bd9949169b1b1d5657d724e2204
AleCostaS <alexandrec.silva@usp.br>
2022-07-27 16:44:47 -0300

    recebendo os filmes da api e criando a base de js das primeiras telas

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
+import Menu from "./components/Menu";
+import Movies from "./components/Movies";
+
+export default function App() {
+    const [movies, setMovies] = useState([]);
+
+    const requisition = axios.get('https://mock-api.driven.com.br/api/v5/cineflex/movies');
+
+    requisition.then(answer => {
+        setMovies(answer.data);   
+    });
+
+    return (
+        <BrowserRouter>
+            <Menu />
+            <Routes>
+                <Route path="/" element={<Movies movies={movies}/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
---
+++ src/components/Movies.js
@@ -0,0 +1,9 @@
+export default function Movies ({ movies }) {
+    console.log(movies);
+    
+    return (
+        <div className='title'>
+            Selecione o filme
+        </div>
+    );
+}
