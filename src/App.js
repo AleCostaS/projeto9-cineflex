99b24eeadd4aecc7b412e4f5967f1fc36783957f
AleCostaS <alexandrec.silva@usp.br>
2022-07-27 18:31:10 -0300

    putting the links of the seats based on the movie

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Menu from "./components/Menu";
 import Movies from "./components/Movies";
 import Session from "./components/Session";
+import Seats from "./components/Seats";
 
 export default function App() {
     const [movies, setMovies] = useState([]);
@@ -24,7 +25,9 @@ export default function App() {
                 {movies.map(movie => {
                     return <Route path={"/sessoes/"+movie.id} element={<Session movie={movie} />}/>
                 })}
-                
+                {movies.map(movie => {
+                    return <Route path={"/assentos/"+movie.id} element={<Seats movie={movie} />}/>
+                })}
             </Routes>
         </BrowserRouter>
     );
---
+++ src/components/Seats.js
@@ -0,0 +1,7 @@
+export default function Seats ({ movie }) {
+    return (
+        <div className='Menu'>
+           <img src={movie.posterURL}/>
+        </div>
+    );
+}
